2d8e153c59f40b1021dbfb889916e7dd1bd82dd1
Youssef Ichda <youssefichda99@gmail.com>
2026-09-05 20:23:40 +0100

    Save the username and password to registry instead of file

--- DVLD Project Version 1.0/Global Classes/clsGlobal.cs
+++ DVLD Project Version 1.0/Global Classes/clsGlobal.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace DVLD_Project_Version_1._0.Global_Classes
 {
@@ -12,34 +13,14 @@ namespace DVLD_Project_Version_1._0.Global_Classes
         public static bool RememberUsernameAndPassword(string Username, string Password)
         {
 
+            string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD";
+
             try
             {
-                //this will get the current project directory folder.
-                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+                Registry.SetValue(keyPath, "Username", Username, RegistryValueKind.String);
+                Registry.SetValue(keyPath, "Password", Password, RegistryValueKind.String);
 
-
-                // Define the path to the text file where you want to save the data
-                string filePath = currentDirectory + "\\data.txt";
-
-                //incase the username is empty, delete the file
-                if (Username == "" && File.Exists(filePath))
-                {
-                    File.Delete(filePath);
-                    return true;
-
-                }
-
-                // concatonate username and passwrod withe seperator.
-                string dataToSave = Username + "#//#" + Password;
-
-                // Create a StreamWriter to write to the file
-                using (StreamWriter writer = new StreamWriter(filePath))
-                {
-                    // Write the data to the file
-                    writer.WriteLine(dataToSave);
-
-                    return true;
-                }
+                return true;
             }
             catch (Exception ex)
             {
@@ -51,38 +32,16 @@ namespace DVLD_Project_Version_1._0.Global_Classes
 
         public static bool GetStoredCredential(ref string Username, ref string Password)
         {
-            //this will get the stored username and password and will return true if found and false if not found.
+
+            string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD";
+
             try
             {
-                //gets the current project's directory
-                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
 
-                // Path for the file that contains the credential.
-                string filePath = currentDirectory + "\\data.txt";
+                Username = Registry.GetValue(keyPath, "UserName", null) as string;
+                Password = Registry.GetValue(keyPath, "Password", null) as string;
 
-                // Check if the file exists before attempting to read it
-                if (File.Exists(filePath))
-                {
-                    // Create a StreamReader to read from the file
-                    using (StreamReader reader = new StreamReader(filePath))
-                    {
-                        // Read data line by line until the end of the file
-                        string line;
-                        while ((line = reader.ReadLine()) != null)
-                        {
-                            Console.WriteLine(line); // Output each line of data to the console
-                            string[] result = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
-
-                            Username = result[0];
-                            Password = result[1];
-                        }
-                        return true;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
+                return true;
             }
             catch (Exception ex)
             {
