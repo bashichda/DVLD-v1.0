28db33a761a4c2dabffcf6ad94f8a6568d23b41a
Youssef Ichda <youssefichda99@gmail.com>
2026-09-07 16:42:05 +0100

    Add App.Config in Presentation Layer and write connectionString on it and replace in clsDataAccessSettings

--- DVLD-DataAccessLayer/clsDataAccessSettings.cs
+++ DVLD-DataAccessLayer/clsDataAccessSettings.cs
@@ -1,13 +1,10 @@
 ﻿using System; using DVLD_Common;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace DVLD_DataAccessLayer
 {
     internal class clsDataAccessSettings
     {
-        public static string connectionString = "Server=.;DataBase=DVLD;User Id=sa;Password=123456;";
+        public static string connectionString = ConfigurationManager.ConnectionStrings["DVLD_DBConnection"].ConnectionString;
     }
 }
