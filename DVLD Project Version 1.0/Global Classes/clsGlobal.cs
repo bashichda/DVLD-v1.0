146a93b9bc2e4d4d43f6de2ff1e77bc06c679262
Youssef Ichda <youssefichda99@gmail.com>
2026-09-10 11:48:07 +0100

    Add Encrypt and Decrypt Function Using Symmetric Encryption in login From Before save in Registry and After Get Value from Registry

--- DVLD Project Version 1.0/Global Classes/clsGlobal.cs
+++ DVLD Project Version 1.0/Global Classes/clsGlobal.cs
@@ -1,9 +1,10 @@
 ﻿using DVLD_BusinessLayer;
-using System; using DVLD_Common;
-using System.IO;
-using System.Windows.Forms;
+using DVLD_Common;
 using Microsoft.Win32;
-using System.Diagnostics;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
 
 namespace DVLD_Project_Version_1._0.Global_Classes
 {
@@ -11,15 +12,19 @@ namespace DVLD_Project_Version_1._0.Global_Classes
     {
         public static clsUser CurrentUser;
 
+        private static string _Key = "1234567890123456";
+
         public static bool RememberUsernameAndPassword(string Username, string Password)
         {
 
             string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD";
 
+            
+
             try
             {
                 Registry.SetValue(keyPath, "Username", Username, RegistryValueKind.String);
-                Registry.SetValue(keyPath, "Password", Password, RegistryValueKind.String);
+                Registry.SetValue(keyPath, "Password", Encrypt(Password,_Key), RegistryValueKind.String);
 
                 return true;
             }
@@ -36,11 +41,14 @@ namespace DVLD_Project_Version_1._0.Global_Classes
 
             string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD";
 
+
+
             try
             {
 
                 Username = Registry.GetValue(keyPath, "UserName", null) as string;
-                Password = Registry.GetValue(keyPath, "Password", null) as string;
+                Password = Decrypt(Registry.GetValue(keyPath, "Password", null) as string, _Key);
+
 
                 return true;
             }
@@ -49,7 +57,59 @@ namespace DVLD_Project_Version_1._0.Global_Classes
                 MessageBox.Show($"An error occurred: {ex.Message}");
                 return false;
             }
-
         }
+
+            public static string Encrypt(string plainText, string key)
+            {
+                using (Aes aesAlg = Aes.Create())
+                {
+                    // Set the key and IV for AES encryption
+                    aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                    aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+
+                    // Create an encryptor
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+
+                    // Encrypt the data
+                    using (var msEncrypt = new System.IO.MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        using (var swEncrypt = new System.IO.StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+
+
+                        // Return the encrypted data as a Base64-encoded string
+                        return Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                }
+            }
+
+            public static string Decrypt(string cipherText, string key)
+            {
+                using (Aes aesAlg = Aes.Create())
+                {
+                    // Set the key and IV for AES decryption
+                    aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                    aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+
+                    // Create a decryptor
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+
+                    // Decrypt the data
+                    using (var msDecrypt = new System.IO.MemoryStream(Convert.FromBase64String(cipherText)))
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    using (var srDecrypt = new System.IO.StreamReader(csDecrypt))
+                    {
+                        // Read the decrypted data from the StreamReader
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
     }
 }
